8976b1ffb66153219a408fcae9d7167155e72f7f
hrwang <hrwang@zowietek.cn>
2015-11-30 16:00:22 +0800

    dir

--- apue/dir/dir.c
+++ apue/dir/dir.c
@@ -9,6 +9,90 @@ int read_dir()
 	int fd;
 	int ret;
 	char buf[50];
+	int dup_fd;
+	FILE* fp;
+	int fd_log;
+
+	ret = fcntl(STDOUT_FILENO, F_GETFL, 0);
+	if (ret < 0) {
+		err_sys("fcntl");
+	}
+	
+	switch (O_ACCMODE & ret) {
+	case O_RDONLY:
+		printf("read only!\n");
+		break;
+
+	case O_WRONLY:
+		printf("write only!\n");
+		break;
+
+	case O_RDWR:
+		printf("read and write\n");
+		break;
+
+	default:
+		break;
+	}
+
+	if (ret & O_APPEND) {
+		printf("open append!\n");
+	}
+
+	if (ret & O_NONBLOCK) {
+		printf("no block!\n");
+	}
+
+	if (ret & O_SYNC) {
+		printf("open sync!\n");
+	}
+
+	fd_log = open("../log_file.txt", O_RDWR | O_CREAT | O_APPEND, 0644);
+	if (fd_log < 0) {
+		err_sys("open");
+	}
+
+	ret = fcntl(fd_log, F_GETFL);
+	if (ret < 0)
+		err_sys("fcntl");
+	
+	if (ret & O_SYNC) {
+		printf("log file O_SYNC is set!\n");
+	} else {
+		printf("log file O_SYNC is not set!\n");
+
+		ret |= O_SYNC;
+		ret = fcntl(fd_log, F_SETFL, ret);
+		if (ret < 0)
+			err_sys("fcntl");
+
+		ret = fcntl(fd_log, F_GETFL);
+		if (ret & O_SYNC) {
+			printf("log file set O_SYNC success!\n");
+		}
+	}
+
+	if (dup2(fd_log, STDOUT_FILENO) < 0) {
+		err_sys("dup2");
+	}
+
+	printf("haha!\n");
+//	close(STDOUT_FILENO);
+	close(fd_log);
+	return 0;
+
+
+// 	dup_fd = dup(STDOUT_FILENO);
+// 	if (dup_fd < 0) {
+// 		err_sys("dup error");
+// 	}
+// 	fp = fdopen(dup_fd, "w");
+// 	if (fp < 0)
+// 		err_sys("fdopen\n");
+// 
+// 	fputs("hello dup!\n", fp);
+// 	fclose(fp);
+
 
 	getcwd(buf, 50);
 	printf("current work directory: %s\n", buf);
